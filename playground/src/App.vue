650d888280180cde0079f6cfef3fb2e54c709e95
angelchavez19 <infoangelchavez@gmail.com>
2025-05-23 11:37:35 -0500

    feat(use-form): reset form prop in submit

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,21 +2,27 @@
 import type { ObjectData } from "../../src/composables/use-form";
 import { useForm } from "../../src/main";
 
-const submit = async (data: ObjectData) => {
+const submit = async (
+  data: ObjectData,
+  reset: (values?: ObjectData) => void
+) => {
   console.log(data);
+
+  reset({
+    url_input: "https://angelchavez-portfolio.vercel.app",
+  });
 };
 
-const form = useForm({ submit });
+const { formProps } = useForm({ submit });
 </script>
 
 <template>
-  <form v-bind="form.formProps">
+  <form v-bind="formProps">
     <input type="email" name="email_input" value="test@example.com" />
     <input type="file" name="file_input" />
     <input type="image" name="image_input" src="example.png" alt="Submit" />
     <input type="number" name="number_input" value="42" />
     <input type="password" name="password_input" value="123456" />
-    <input type="reset" name="reset_input" value="Reset" />
     <input type="search" name="search_input" value="query" />
     <input type="submit" name="submit_input" value="Submit" />
     <input type="tel" name="tel_input" value="+1234567890" />
--- src/composables/use-form.ts
+++ src/composables/use-form.ts
@@ -6,11 +6,35 @@ export type ObjectData = Record<string, unknown>;
 export interface UseFormProps extends FormHTMLAttributes {
   disabled?: MaybeRefOrGetter<boolean>;
   disableHtmlValidation?: boolean;
-  submit?: (data: ObjectData) => MaybePromise<void>;
+  submit?: (
+    data: ObjectData,
+    reset: (values?: ObjectData) => void
+  ) => MaybePromise<void>;
 }
 
 export const useForm = (props?: UseFormProps) => {
   const id = props?.id || useId();
+
+  const reset = (values?: ObjectData) => {
+    const form = document.getElementById(id) as HTMLFormElement;
+    if (!form) return;
+
+    const elements = Array.from(form.elements);
+    for (let element of elements) {
+      if (
+        element instanceof HTMLInputElement ||
+        element instanceof HTMLSelectElement ||
+        element instanceof HTMLTextAreaElement ||
+        element instanceof HTMLButtonElement
+      ) {
+        if (values && values[element.name] !== undefined) {
+          element.value = values[element.name] as string;
+        } else {
+          element.value = "";
+        }
+      }
+    }
+  };
 
   const handleSubmit = (payload: Event) => {
     payload.preventDefault();
@@ -35,7 +59,7 @@ export const useForm = (props?: UseFormProps) => {
       }
     }
 
-    const submit = props.submit(data);
+    const submit = props.submit(data, reset);
 
     if (submit instanceof Promise) {
       submit.catch((error) => {
